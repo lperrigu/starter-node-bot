a0fe36609fc8e2f1663fddc40dbecc6240db8ff4
Leo Perriguey <lperrigu@student.42.fr>
2016-08-08 14:23:20 -0700

    Test change response

--- index.js
+++ index.js
@@ -72,5 +72,5 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function 
 })
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand LOLOL. \n')
 })
