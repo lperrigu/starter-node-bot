4fb7779f3e5df9a48b1dfd1683a287a96cbcc6c2
Leo Perriguey <lperrigu@student.42.fr>
2016-08-12 10:33:58 -0700

    Back to the basism add interactive_message_callback

--- index.js
+++ index.js
@@ -81,6 +81,45 @@ controller.hears('interactive', 'direct_message', function(bot, message) {
 });
 
 
+controller.on('interactive_message_callback', function(bot, message) {
+
+    // check message.actions and message.callback_id to see what action to take...
+
+    bot.replyInteractive(message, {
+	text: '...',
+	attachments: [
+	                {
+			    title: 'My buttons',
+			    callback_id: '123',
+			    attachment_type: 'default',
+			    actions: [
+				                    {
+							"name":"yes",
+							"text": "Yes!",
+							"value": "yes",
+							"type": "button",
+						    },
+				{
+				    "text": "No!",
+				    "name": "no",
+				    "value": "delete",
+				    "style": "danger",
+				    "type": "button",
+				    "confirm": {
+					"title": "Are you sure?",
+					"text": "This will do something!",
+					"ok_text": "Yes",
+					"dismiss_text": "No"
+				    }
+				}
+			    ]
+			}
+	]
+    });
+
+});
+
+
 controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
 				 'direct_message,direct_mention,mention', function(bot, message) {
 
