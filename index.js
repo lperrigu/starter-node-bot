9ae16442febdf6e275b3277b902a692ad9399817
Leo Perriguey <lperrigu@student.42.fr>
2016-08-12 10:01:22 -0700

    Add interactive to controller.hears

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ function (bot, message)
 })
 */
 
-controller.hears(['quiz', '!quiz', '!q'], ['direct_message'],
+controller.hears('interactive', ['quiz', '!quiz', '!q'], ['direct_message'],
 		 function (bot, message)
 		 {
 //		     bot.reply(message, 'OK, let\'s make a little quiz')
