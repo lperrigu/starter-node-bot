74ab465f1153aad8e649a005b051b7b72fc0751c
Leo Perriguey <lperrigu@student.42.fr>
2016-08-13 21:16:28 -0700

    Test config test change scopes

--- index.js
+++ index.js
@@ -23,14 +23,15 @@ var controller = Botkit.slackbot({
 
 
 
-/*
+
 controller.configureSlackApp({
     clientId: process.env.clientId,
     clientSecret: process.env.clientSecret,
     redirectUri: 'http://localhost:3002',
-    scopes: ['incoming-webhook','team:read','users:read','channels:read','im:read','im:write','groups:read','emoji:read','chat:write:bot']
+    //    scopes: ['incoming-webhook','team:read','users:read','channels:read','im:read','im:write','groups:read','emoji:read','chat:write:bot']
+    scopes: ['bot']
 });
-
+/*
 controller.setupWebserver(process.env.port,function(err,webserver) {
 
     // set up web endpoints for oauth, receiving webhooks, etc.
